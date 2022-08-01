6a83adf255067d3d173578c8de0ce1474a35d98d
Adrian7172 <amnchauhan7172@gmail.com>
2022-08-01 12:38:19 +0530

    fixed-bug

--- src/components/Top/Navbar.tsx
+++ src/components/Top/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Disclosure} from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const navigation = [
@@ -33,12 +33,12 @@ const Navbar = () => {
     >
       {({ open }) => (
         <>
-          <div
-            className={`max-w-[1260px] mx-auto px-2 sm:px-6 lg:px-8 ${
-              scroll ? "py-3 md:py-0 " : " py-6"
-            } `}
-          >
-            <div className="relative flex items-center justify-between h-5 md:h-16">
+          <div className={` max-w-[1260px] mx-auto px-2 sm:px-6 lg:px-8 ${
+                scroll ? "py-4 md:py-0 " : " py-6"
+              } `}>
+            <div
+              className={` relative flex items-center justify-between h-5 md:h-16`}
+            >
               <div className="absolute inset-y-0 left-0 flex items-center">
                 <div
                   className={` text-[26px] font-black ${
--- src/components/Top/index.tsx
+++ src/components/Top/index.tsx
@@ -5,10 +5,11 @@ import Navbar from "./Navbar";
 const TopComponent = () => {
   return (
     <div className="bg-hero-pattern h-[100vh] bg-cover bg-little-top bg-no-repeat relative">
-      <div className="w-full h-full bg-hero-gadient opacity-80 absolute z-0">
-      </div>
+      <div className="w-full h-full bg-hero-gadient opacity-80 absolute z-0"></div>
+      <div className="w-full h-full opacity-100 absolute">
         <Navbar />
         <Intro />
+      </div>
     </div>
   );
 };

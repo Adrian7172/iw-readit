b7242f5c8d8649a147222013ef83bb26da88e167
Adrian7172 <amnchauhan7172@gmail.com>
2022-08-01 13:59:16 +0530

    fixed Intro

--- src/components/Top/Intro.tsx
+++ src/components/Top/Intro.tsx
@@ -8,7 +8,7 @@ const Intro = () => {
         <h3 className="text-[18px] font-normal text-white z-10">Hello! Welcome to</h3>
         <h1 className="text-white text-[10vw] font-bold lg:mt-[-22px] mb-4 md:mb-0">Readit blog</h1>
 
-        <h3 className="text-[18px] font-normal text-white pr-4 md:w-[650px] lg:mt-[-15px]">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</h3>
+        <h3 className="text-[18px] font-normal text-white pr-4 md:w-[660px] lg:mt-[-15px]">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</h3>
         <div className="">
             <CgArrowDown  className="text-white mt-10 w-12 h-12 animate-bounce"/>
         </div>

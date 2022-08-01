9a2f6e9bc9b3fa6bc1f01cbacf02d4c2acd66c5a
Adrian7172 <amnchauhan7172@gmail.com>
2022-08-01 11:06:36 +0530

    fix Navbar

--- src/components/Top/Navbar.tsx
+++ src/components/Top/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
       as="nav"
       className={` ${
         scroll
-          ? "fixed w-full top-0 bg-white transition-all ease-in bg-fixed"
+          ? "fixed w-full top-0 bg-white transition-all ease-in bg-fixed z-100"
           : "w-full bg-black md:bg-transparent transition-all ease-in bg-fixed"
       }`}
     >
@@ -36,7 +36,7 @@ const Navbar = () => {
         <>
           <div
             className={`max-w-[1260px] mx-auto px-2 sm:px-6 lg:px-8 ${
-              scroll ? "py-3 md:py-0" : " py-6"
+              scroll ? "py-3 md:py-0 " : " py-6"
             } `}
           >
             <div className="relative flex items-center justify-between h-5 md:h-16">
@@ -51,7 +51,11 @@ const Navbar = () => {
               </div>
 
               <div className="absolute inset-y-0 right-0 flex items-center pr-2">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white   md:hidden sm:block">
+                <Disclosure.Button
+                  className={`inline-flex items-center justify-center p-2 rounded-md ${
+                    scroll ? "text-black" : "text-text-color6"
+                  }  hover:text-white   md:hidden sm:block`}
+                >
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -88,7 +92,7 @@ const Navbar = () => {
           </div>
 
           <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
+            <div className="px-2 pt-2 pb-3 space-y-1 flex flex-col">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
@@ -96,8 +100,12 @@ const Navbar = () => {
                   href={item.href}
                   className={
                     item.current
-                      ? "text-white px-3 py-2 rounded-md text-base font-medium"
-                      : "text-gray-400 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                      ? ` ${
+                          scroll ? "text-yellow-300" : "text-white"
+                        } px-3 py-2 rounded-md text-base font-medium `
+                      : ` ${
+                          scroll ? "text-black" : "text-nav-item"
+                        } px-3 py-2 rounded-md text-[18px] text-base font-medium`
                   }
                   aria-current={item.current ? "page" : undefined}
                 >
